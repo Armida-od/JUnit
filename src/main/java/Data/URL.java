9364616beffa904abab3269bfa09dff4344fdaa8
armida <ziablova25@gmail.com>
2023-05-13 20:44:01 +0300

    1: JUnit-1

---
+++ src/main/java/Data/URL.java
@@ -0,0 +1,15 @@
+//package Data;
+//
+//public enum URL {
+//    MAIN_PAGE ("https://ithillel.ua/");
+//
+//    private String url;
+//    URL(String url){
+//        this.url = url;
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return url;
+//    }
+//}
